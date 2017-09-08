1fbd3dd0648bd46c796bd205dacf696c19b2a8b6
Harris Schneiderman <schne324@gmail.com>
2017-09-07 22:30:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Axe Headless Chrome API
+Runs axe on a page in [puppeteer](https://github.com/GoogleChrome/puppeteer), a headless chrome browser.
+
+
+## API
+### `?url=http://example.com`
+the "url" query parameter accepts any valid url
---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+require('koa-qs')(app);
+
+router.get('/', async (ctx) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(ctx.query.url || 'https://google.com');
+  // add axe to page
+  await page.addScriptTag('https://unpkg.com/axe-core');
+  // run axe on the page
+  const results = await page.evaluate(async () => {
+    return await axe.run();
+  });
+
+  browser.close();
+
+  ctx.body = results;
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(3000);
